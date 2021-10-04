43b5098c72d0561ece8dfea5c6688562ecdbbe5b
sosow0212 <sosow0212@naver.com>
2021-10-05 00:13:59 +0900

    q11654 - 수학 - 아스키코드 ASCHII

---
+++ src/com/sosow0212/q11654.java
@@ -0,0 +1,11 @@
+package com.sosow0212;
+
+import java.util.Scanner;
+
+public class q11654 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int input = sc.next().charAt(0);
+        System.out.println(input);
+    }
+}
