574091fec75f986ef389328d745bdf2741632b57
sosow0212 <sosow0212@naver.com>
2021-10-05 00:04:49 +0900

    q1550 수학 - 16진수 변환

---
+++ src/com/sosow0212/q1550.java
@@ -0,0 +1,12 @@
+package com.sosow0212;
+
+import java.util.*;
+
+public class q1550 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.next();
+        System.out.println(Integer.parseInt(input,16));
+
+    }
+}
