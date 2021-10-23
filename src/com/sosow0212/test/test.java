a002297c572145b4773e08381304b3162f6f3ff0
sosow0212 <sosow0212@naver.com>
2021-10-23 13:02:54 +0900

    .

--- src/com/sosow0212/test/test.java
+++ src/com/sosow0212/test/test.java
@@ -25,7 +25,6 @@ class Solution {
                 }
             }
         }
-
         return answer;
     }
 
---
+++ src/com/sosow0212/프로그래머스/Main.java
@@ -0,0 +1,10 @@
+package com.sosow0212.프로그래머스;
+
+public class Main {
+    public static void main(String[] args) {
+        String a = "6";
+        String b = "3";
+
+        System.out.println(a+b+"d");
+    }
+}
