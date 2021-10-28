a3301ad72a69d7ff394a526b887281e3dc648bde
이재윤 <63213487+sosow0212@users.noreply.github.com>
2021-10-28 11:48:27 +0900

    .

---
+++ q2420.java
@@ -0,0 +1,15 @@
+package study;
+
+import java.util.*;
+public class q2420 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		long n = sc.nextLong();
+		long m = sc.nextLong();
+		
+		System.out.println(Math.abs(n-m));
+	}
+
+}
