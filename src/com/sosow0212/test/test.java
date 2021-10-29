bc84e40a832f70bbe2a8a0827d6e006679262866
sosow0212 <sosow0212@naver.com>
2021-10-29 11:10:23 +0900

    .

--- src/com/sosow0212/test/test.java
+++ src/com/sosow0212/test/test.java
@@ -4,30 +4,7 @@ import java.util.HashMap;
 
 public class test {
     public static void main(String[] args) {
-
-    }
-}
-
-class Solution {
-    public boolean solution(String[] phone_book) {
-        boolean answer = true;
-
-        HashMap<String, String> hashMap = new HashMap<String, String>();
-
-        for (int i = 0; i < phone_book.length; i++) {
-            hashMap.put(phone_book[i], " ");
-        }
-
-        for (String p : phone_book) {
-            for (int i = 0; i < p.length(); i++) {
-                if(hashMap.containsKey(p.substring(0,i))) {
-                    return false;
-                }
-            }
-        }
-        return answer;
-    }
-
-    private class Hashmap<T, T1> {
+        int[] arr = new int[3];
+        System.out.println(arr.length);
     }
 }
