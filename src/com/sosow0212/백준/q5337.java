bed08898502b9ea72f67bd046a6d251725619c81
sosow0212 <sosow0212@naver.com>
2021-10-30 18:51:25 +0900

    .

---
+++ src/com/sosow0212/백준/q5337.java
@@ -0,0 +1,9 @@
+package com.sosow0212.백준;
+
+public class q5337 {
+    public static void main(String[] args) {
+        System.out.println(".  .   .\n" +
+                "|  | _ | _. _ ._ _  _\n" +
+                "|/\\|(/.|(_.(_)[ | )(/.");
+    }
+}
