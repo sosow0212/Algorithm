4af74ff4acbd1009ff3a5dada5c0bd34ed483235
sosow0212 <sosow0212@naver.com>
2021-10-30 18:56:22 +0900

    .

---
+++ src/com/sosow0212/백준/q11382.java
@@ -0,0 +1,14 @@
+package com.sosow0212.백준;
+
+import java.util.Scanner;
+
+public class q11382 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long a = sc.nextLong();
+        long b = sc.nextLong();
+        long c = sc.nextLong();
+
+        System.out.println(a + b + c);
+    }
+}
