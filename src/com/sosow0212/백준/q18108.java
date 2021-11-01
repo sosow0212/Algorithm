f27c9c3368014fa0996c0be86f2819ce34f45aa6
이재윤 <63213487+sosow0212@users.noreply.github.com>
2021-11-01 13:54:38 +0900

    Add files via upload

---
+++ src/com/sosow0212/백준/q18108.java
@@ -0,0 +1,13 @@
+package study;
+
+import java.util.Scanner;
+
+public class q18108 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int y = sc.nextInt();
+		System.out.println(y - (2541-1998));
+	}
+
+}
