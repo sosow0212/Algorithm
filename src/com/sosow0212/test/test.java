5e7859c898491fb4087da51d519a87fd812e0e13
sosow0212 <sosow0212@naver.com>
2021-11-04 19:44:43 +0900

    .

--- src/com/sosow0212/test/test.java
+++ src/com/sosow0212/test/test.java
@@ -6,5 +6,7 @@ public class test {
     public static void main(String[] args) {
         int[] arr = {1,2,3,4,5,6,7,8,9};
         int[] arr2 = {1,2,3};
+        String str = "MindMap";
+        System.out.println(str.toLowerCase());
     }
 }
