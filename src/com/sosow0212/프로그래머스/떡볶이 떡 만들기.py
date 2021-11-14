b07ed6a41c3506632ad96c66ba4d5b68383f36f0
이재윤 <63213487+sosow0212@users.noreply.github.com>
2021-11-14 18:09:59 +0900

    Add files via upload

---
+++ src/com/sosow0212/프로그래머스/떡볶이 떡 만들기.py
@@ -0,0 +1,25 @@
+# 떡의 개수 n, 주문한 떡의 길이 m
+n, m = list(map(int, input().split()))
+array = list(map(int, input().split()))
+
+start = 0
+end = max(array)
+result = 0
+
+while(start <= end):
+    total = 0
+    mid = (start + end) // 2
+
+    for x in array: #잘랐을 때 떡의 양 계산
+        if x > mid:
+            total += x - mid
+            
+    #떡의 양이 부족한 경우 더 많이 자르기 (왼쪽 탐색)
+    if total < m:
+        end = mid - 1
+    else:
+        result = mid
+        #최대한 덜 자를때가 정답이므로, 여기서 result에 기록
+        start = mid + 1
+
+print(result)
