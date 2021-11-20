2fbd8d54ccea37fcb3d0f716fdcd93d09fe6e220
sosow0212 <sosow0212@naver.com>
2021-11-20 12:28:51 +0900

    .

---
+++ src/com/sosow0212/백준/q11719.java
@@ -0,0 +1,15 @@
+package com.sosow0212.백준;
+
+import java.util.Scanner;
+
+public class q11719 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNextLine()) {
+            String input = sc.nextLine();
+            System.out.println(input);
+        }
+        sc.close();
+    }
+}
