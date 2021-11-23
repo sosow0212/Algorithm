e9ad66770fdedd0e03cfecd1010052fafa2a49bb
이재윤 <63213487+sosow0212@users.noreply.github.com>
2021-11-23 09:27:51 +0900

    Add files via upload

---
+++ src/com/sosow0212/백준/q2744.java
@@ -0,0 +1,25 @@
+package study;
+
+import java.util.*;
+
+public class q2744 {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    String str = scanner.nextLine();
+
+    char[] arr;
+    arr = str.toCharArray();
+
+    for(int i = 0; i < arr.length; i++) {
+      if(65 <= arr[i] && arr[i] <= 90) {
+        arr[i] = (char) (arr[i] + 32);
+        continue;
+      }
+      if(97 <= arr[i] && arr[i] <= 122) {
+        arr[i] = (char) (arr[i] - 32);
+        continue;
+      }
+    }
+    System.out.println(arr);
+  }
+}
