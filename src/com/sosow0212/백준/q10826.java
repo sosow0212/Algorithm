9ba708dc3cbf8a3f0cca31ca8b7fa448a5222e8e
sosow0212 <sosow0212@naver.com>
2021-11-28 18:40:26 +0900

    .

---
+++ src/com/sosow0212/백준/q10826.java
@@ -0,0 +1,33 @@
+package com.sosow0212.백준;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.math.BigInteger;
+
+public class q10826 {
+    static BigInteger arr[];
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+
+        arr = new BigInteger[n + 2];
+        arr[0] = BigInteger.ZERO;
+        arr[1] = BigInteger.ONE;
+
+        for (int i = 2; i <= n; i++) {
+            arr[i] = arr[i - 2].add(arr[i - 1]);
+        }
+
+        bw.write(String.valueOf(arr[n]));
+
+        bw.flush();
+        br.close();
+        bw.close();
+    }
+}
