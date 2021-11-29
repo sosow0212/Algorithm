9419152f42539a8543197ab884ca64a24c03329b
sosow0212 <sosow0212@naver.com>
2021-11-29 18:48:43 +0900

    .

---
+++ src/com/sosow0212/백준/q14918.java
@@ -0,0 +1,13 @@
+package com.sosow0212.백준;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class q14918 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Integer a = sc.nextInt();
+        Integer b = sc.nextInt();
+        System.out.println((a+b));
+    }
+}
