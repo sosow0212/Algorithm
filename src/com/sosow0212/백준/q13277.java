8d303cf35f4c71327e3a4ed32c09bfa3290ca7ea
이재윤 <63213487+sosow0212@users.noreply.github.com>
2021-11-30 10:50:08 +0900

    Add files via upload

---
+++ src/com/sosow0212/백준/q13277.java
@@ -0,0 +1,13 @@
+package study;
+import java.util.*;
+public class q13277 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		Integer a = sc.nextInt();
+		Integer b = sc.nextInt();
+		System.out.println(a*b);
+
+	}
+
+}
