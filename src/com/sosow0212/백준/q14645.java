81f66b76496abdef9bbcd5cfa4c28fdc2e4dd083
이재윤 <63213487+sosow0212@users.noreply.github.com>
2021-12-01 11:54:43 +0900

    Add files via upload

---
+++ src/com/sosow0212/백준/q14645.java
@@ -0,0 +1,18 @@
+package study;
+
+import java.util.*;
+
+public class q14645 {
+
+public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+	int a = sc.nextInt();
+	int b = sc.nextInt();
+	for(int i=0;i<a;i++)
+	{
+		int one = sc.nextInt();
+		int two = sc.nextInt();
+	}
+	System.out.print("ºñ¿ÍÀÌ");
+	}
+}
