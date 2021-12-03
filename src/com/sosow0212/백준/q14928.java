f450f128d653a29df1295b61f045bca6d751eebd
Jaeyoon Lee <63213487+sosow0212@users.noreply.github.com>
2021-12-03 10:21:04 +0900

    Add files via upload

---
+++ src/com/sosow0212/백준/q14928.java
@@ -0,0 +1,14 @@
+package study;
+
+import java.math.BigInteger;
+import java.util.*;
+
+public class q14928 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		BigInteger n = sc.nextBigInteger();
+		System.out.println(n.remainder(BigInteger.valueOf(20000303)));
+	}
+
+}
