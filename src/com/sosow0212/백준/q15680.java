0f70502068f8360e2027b19a93cb777eaa6a3e68
sosow0212 <sosow0212@naver.com>
2021-12-05 11:28:22 +0900

    .

---
+++ src/com/sosow0212/백준/q15680.java
@@ -0,0 +1,15 @@
+package com.sosow0212.백준;
+
+import java.util.Scanner;
+
+public class q15680 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        if(n == 0) {
+            System.out.println("YONSEI");
+        } else {
+            System.out.println("Leading the Way to the Future");
+        }
+    }
+}
