d771ef77efdfcac36ccc0a5965ae361723920ffb
sosow0212 <sosow0212@naver.com>
2021-12-07 15:02:48 +0900

    .

---
+++ src/com/sosow0212/백준/q2523.java
@@ -0,0 +1,30 @@
+package com.sosow0212.백준;
+
+import java.util.Scanner;
+
+public class q2523 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+
+        for (int i = n - 1; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+
+}
