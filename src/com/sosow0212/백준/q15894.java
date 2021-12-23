8338bb3594d7a6d761fe4a4864c2ab3fd7937cf3
Jaeyoon Lee <63213487+sosow0212@users.noreply.github.com>
2021-12-23 10:44:55 +0900

    Add files via upload

---
+++ src/com/sosow0212/백준/q15894.java
@@ -0,0 +1,13 @@
+package study;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class q15894 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		System.out.print(Long.parseLong(br.readLine()) * 4);
+	}
+}
