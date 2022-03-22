d0806c82ebaa9f156215130ea1fdeb46325b5578
sosow0212 <sosow0212@naver.com>
2022-03-22 16:47:38 +0900

    .

--- src/com/sosow0212/codeUp/q4766.java
+++ src/com/sosow0212/codeUp/q4766.java
@@ -8,41 +8,35 @@ public class q4766 {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int[] arr = new int[n];
-
         for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
         }
-        Arrays.sort(arr);
+        int k = sc.nextInt();
 
-        int m = sc.nextInt(); // 총 예산
-        // 110 120 140 150 // 130
-        // 110 120 130 130 = 490
-
-        int high = arr[n-1];
         int low = 0;
-        int mid;
-        int result = 0;
+        int high = arr[arr.length-1];
+        int mid = 0;
+        int sum;
         int ans = 0;
 
-        while (low <= high) {
+        while(low <= high) {
             mid = (low + high) / 2;
-            result = 0;
+            sum = 0;
 
             for(int i=0; i<n; i++) {
                 if(arr[i] >= mid) {
-                    result += mid;
+                    sum += mid;
                 } else {
-                    result += arr[i];
+                    sum += arr[i];
                 }
             }
 
-            if(result > m) {
+            if(sum > k) {
                 high = mid - 1;
             } else {
                 ans = mid;
                 low = mid + 1;
             }
-
         }
 
         System.out.println(ans);
