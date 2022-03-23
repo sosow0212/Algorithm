b737acc561716843422a323268f570c1c5720eb8
sosow0212 <sosow0212@naver.com>
2022-03-23 18:53:02 +0900

    .

---
+++ src/com/sosow0212/백준/q16486.java
@@ -0,0 +1,14 @@
+package com.sosow0212.백준;
+
+import java.util.Scanner;
+
+public class q16486 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double a = sc.nextInt();
+        double b = sc.nextDouble();
+
+        double result = 2 * a + 3.141592 * 2 * b;
+        System.out.println(result);
+    }
+}
