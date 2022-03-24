2a61b3b77de57b79687ce9733969970ba5eb43ea
sosow0212 <sosow0212@naver.com>
2022-03-24 15:14:24 +0900

    .

---
+++ src/com/sosow0212/백준/q2579.java
@@ -0,0 +1,28 @@
+package com.sosow0212.백준;
+
+import java.util.Scanner;
+
+public class q2579 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int[] arr = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int[] dp = new int[n + 1];
+        dp[1] = arr[1];
+        dp[2] = arr[1] + arr[2];
+
+        for (int i = 3; i <= n; i++) {
+            dp[i] = Math.max(dp[i - 2], dp[i - 3] + arr[i - 1]) + arr[i];
+        }
+
+        System.out.println(dp[n]);
+
+//        10, (10+20, 20), (n-1 + n, n-2 +n)
+    }
+}
