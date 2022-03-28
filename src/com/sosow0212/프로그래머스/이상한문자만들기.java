5a5ee598c33e19ec40beb0901ca53d4fe2efa250
sosow0212 <sosow0212@naver.com>
2022-03-28 16:25:33 +0900

    .

---
+++ src/com/sosow0212/프로그래머스/이상한문자만들기.java
@@ -0,0 +1,23 @@
+package com.sosow0212.프로그래머스;
+
+public class 이상한문자만들기 {
+    public String solution(String s) {
+        String answer = "";
+        int cnt = 0;
+        String[] arr = s.split("");
+
+        for (String str : arr) {
+            cnt = str.contains(" ") ? 0 : cnt + 1;
+            answer += cnt % 2 == 0 ? str.toLowerCase() : str.toUpperCase();
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        이상한문자만들기 sol = new 이상한문자만들기();
+
+        String s = "try hello world";
+        System.out.println(sol.solution(s));
+    }
+}
