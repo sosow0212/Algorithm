354d7dac6e5eaf91a070e269df303e0150c0bf54
sosow0212 <sosow0212@naver.com>
2022-03-29 22:07:18 +0900

    .

---
+++ src/com/sosow0212/프로그래머스/전화번호목록2.java
@@ -0,0 +1,34 @@
+package com.sosow0212.프로그래머스;
+
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class 전화번호목록2 {
+    public boolean solution(String[] phone_book) {
+        boolean answer = true;
+
+        Arrays.sort(phone_book);
+        HashMap<String, String> hashMap = new HashMap<>();
+
+        for(String str : phone_book) {
+            hashMap.put(str, " ");
+        }
+
+        for(String str : phone_book) {
+            for(int i=0; i<str.length(); i++) {
+                if(hashMap.containsKey(str.substring(0, i))) {
+                    return false;
+                }
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        전화번호목록2 sol = new 전화번호목록2();
+
+        String[] phone_book = {"119", "97674223", "1195524421"};
+        System.out.println(sol.solution(phone_book));
+    }
+}
