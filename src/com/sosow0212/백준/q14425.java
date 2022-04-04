35575aa511928f566fc51d31bbd9df5175759b7f
sosow0212 <sosow0212@naver.com>
2022-04-04 13:50:20 +0900

    .

--- src/com/sosow0212/백준/q14425.java
+++ src/com/sosow0212/백준/q14425.java
@@ -1,5 +1,6 @@
 package com.sosow0212.백준;
 
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class q14425 {
@@ -7,9 +8,26 @@ public class q14425 {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int m = sc.nextInt();
+        int count = 0;
+
+        HashMap<String, Integer> hashMap1 = new HashMap();
+        String[] list = new String[m];
 
         for(int i=0; i<n; i++) {
-
+            String str = sc.next();
+            hashMap1.put(str, 0);
         }
+
+        for(int i=0; i<m; i++) {
+            list[i] = sc.next();
+        }
+
+        for(String str : list) {
+            if(hashMap1.containsKey(str)) {
+                count ++;
+            }
+        }
+        System.out.println(count);
+
     }
 }
