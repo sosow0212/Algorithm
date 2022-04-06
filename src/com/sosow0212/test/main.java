cffe9154cf78ec87c61d81b53583cdc9e574e147
sosow0212 <sosow0212@naver.com>
2022-04-06 10:14:52 +0900

    .

--- src/com/sosow0212/test/main.java
+++
@@ -1,8 +0,0 @@
-package com.sosow0212.test;
-
-public class main {
-    public static void main(String[] args) {
-        String str = "123";
-        System.out.println(str.substring(0,1));
-    }
-}
