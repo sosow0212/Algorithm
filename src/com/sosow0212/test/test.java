c8b485b036ebbcf071b11f92dd57508c54183505
sosow0212 <sosow0212@naver.com>
2022-04-07 19:13:41 +0900

    .

--- src/com/sosow0212/test/test.java
+++ src/com/sosow0212/test/test.java
@@ -1,12 +1,27 @@
 package com.sosow0212.test;
 
 import java.util.HashMap;
+import java.util.Scanner;
 
 public class test {
     public static void main(String[] args) {
-        int[] arr = {1,2,3,4,5,6,7,8,9};
-        int[] arr2 = {1,2,3};
-        String str = "MindMap";
-        System.out.println(str.toLowerCase());
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        boolean Value = false;
+
+        // 11 루트 = 3
+        // 2, 3
+        for(int i=2; i<=Math.sqrt(n); i++) {
+            if(n % i == 0) {
+                Value = true;
+            }
+        }
+
+
+        System.out.println(Value);
+
+        // false 일 경우 소수
+        // true 일 경우 소수 아님
     }
 }
