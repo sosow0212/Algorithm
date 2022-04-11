3225ea06fa079e868bb431a49cf66457417fbe4b
sosow0212 <sosow0212@naver.com>
2022-04-11 11:07:42 +0900

    .

---
+++ src/com/sosow0212/백준/q11653.java
@@ -0,0 +1,22 @@
+package com.sosow0212.백준;
+
+import java.util.Scanner;
+
+public class q11653 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            while (n % i == 0) {
+                System.out.println(i);
+                n /= i;
+            }
+        }
+
+        if (n != 1) {
+            System.out.println(n);
+        }
+    }
+}
