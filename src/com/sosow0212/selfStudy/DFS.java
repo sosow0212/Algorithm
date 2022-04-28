a7acc897bee75cc966fe8e49a188894c92d0a98e
sosow0212 <sosow0212@naver.com>
2022-04-28 21:52:51 +0900

    .

--- src/com/sosow0212/selfStudy/DFS.java
+++ src/com/sosow0212/selfStudy/DFS.java
@@ -5,9 +5,9 @@ import java.util.*;
 public class DFS {
 
     static int n;
-    static int[][] graph;
-    static boolean[][] visited;
-    static int[][] pos = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+    static int graph[][];
+    static boolean visited[][];
+    static int[][] pos = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}}; // 상하좌우
     static int count;
 
     public static void main(String[] args) {
@@ -17,42 +17,41 @@ public class DFS {
         graph = new int[n][n];
         visited = new boolean[n][n];
 
-        ArrayList<Integer> list = new ArrayList<>();
-
         for (int i = 0; i < n; i++) {
             String str = sc.next();
             for (int j = 0; j < n; j++) {
                 graph[i][j] = str.charAt(j) - '0';
             }
         }
+        ArrayList<Integer> result = new ArrayList<>();
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
+                count = 0;
                 if (graph[i][j] == 1 && !visited[i][j]) {
-                    count = 0;
                     dfs(i, j);
-                    list.add(count);
+                    result.add(count);
                 }
             }
         }
 
-        System.out.println(list.size());
-        Collections.sort(list);
-        for (int i = 0; i < list.size(); i++) {
-            System.out.println(list.get(i));
+        System.out.println(result.size());
+        Collections.sort(result);
+        for (int i = 0; i < result.size(); i++) {
+            System.out.println(result.get(i));
         }
-
     }
 
     static void dfs(int r, int c) {
-        visited[r][c] = true;
         count++;
+        visited[r][c] = true;
 
         for (int i = 0; i < pos.length; i++) {
+            // 상하좌우 탐색! 총 4번 실행
             int nr = r + pos[i][0];
             int nc = c + pos[i][1];
-
             if (nr >= 0 && nr < n && nc >= 0 && nc < n && graph[nr][nc] == 1 && !visited[nr][nc]) {
+                // 위에 조건이 맞으면 dfs
                 dfs(nr, nc);
             }
         }
