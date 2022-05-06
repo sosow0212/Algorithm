fa9e9535dd8fe407dd9f1ee07385ac5ce78dfa80
sosow0212 <sosow0212@naver.com>
2022-05-06 14:36:38 +0900

    .

---
+++ src/com/sosow0212/백준/q15904.java
@@ -0,0 +1,23 @@
+package com.sosow0212.백준;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class q15904 {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String arr = br.readLine();
+        char[] UCPC = {'U', 'C', 'P', 'C'};
+        int idx = 0;
+        for(int i = 0; i<arr.length(); i++) {
+            if(UCPC[idx] == (arr.charAt(i))) {
+                idx++;
+            }
+            if(idx == 4) {
+                System.out.println("I love UCPC");
+                return;
+            }
+        }
+        System.out.println("I hate UCPC");
+    }
+}
