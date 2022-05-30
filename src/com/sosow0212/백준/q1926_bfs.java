c7b88d61722fbbc361e7cdddfe860c97c7355eda
sosow0212 <sosow0212@naver.com>
2022-05-30 11:03:52 +0900

    .

--- src/com/sosow0212/백준/q1926_bfs.java
+++ src/com/sosow0212/백준/q1926_bfs.java
@@ -11,7 +11,7 @@ public class q1926_bfs {
 
     static void bfs(int x, int y) {
         visited[x][y] = true;
-        // true로 해야지 방문 처리
+        // true로 해야지 방문
         Queue<int[]> queue = new LinkedList<>();
         queue.add(new int[]{x, y});
 
