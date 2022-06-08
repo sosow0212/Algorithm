b3259b80e60c175f1e5ffb4bd7dbee5ca8240178
sosow0212 <sosow0212@naver.com>
2022-06-08 11:29:52 +0900

    .

--- src/com/sosow0212/백준/q2309.java
+++ src/com/sosow0212/백준/q2309.java
@@ -17,7 +17,7 @@ public class q2309 {
             high[i] = sc.nextInt();
             sum += high[i]; // sum은 난쟁이 9명의 키의 합
         }
-        Arrays.sort(high); //키를 오름순서로 정렬
+        Arrays.sort(high); //키를 오름순서로 정렬.
 
         for(int a = 0; a < high.length-1; a++) { //브루트 포스
             for(int b = a+1; b < high.length; b++) {
