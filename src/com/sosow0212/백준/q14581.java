e9b383a01a3517c44eb8bfa68a2d310e053fc895
sosow0212 <sosow0212@naver.com>
2022-09-01 10:06:30 +0900

    .

---
+++ src/com/sosow0212/백준/q14581.java
@@ -0,0 +1,14 @@
+package com.sosow0212.백준;
+
+import java.util.Scanner;
+
+public class q14581 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        System.out.println(":fan::fan::fan:");
+        System.out.println(":fan::" + str +"::fan:");
+        System.out.println(":fan::fan::fan:");
+
+    }
+}
