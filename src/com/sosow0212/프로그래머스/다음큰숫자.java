196d0cc08f34aa80fca6043374114aa3b680a580
sosow0212 <sosow0212@naver.com>
2022-09-27 11:24:59 +0900

    .

---
+++ src/com/sosow0212/프로그래머스/다음큰숫자.java
@@ -0,0 +1,35 @@
+package com.sosow0212.프로그래머스;
+
+public class 다음큰숫자 {
+    static public int solution(int n) {
+        int answer = 0;
+
+        String binaryN = Integer.toBinaryString(n);
+        int countOfN = countOfOne(binaryN);
+
+        int num = n + 1;
+        while (true) {
+            if (countOfOne(Integer.toBinaryString(num)) == countOfN) {
+                answer = num;
+                break;
+            }
+            num++;
+        }
+
+        return answer;
+    }
+
+    static int countOfOne(String binaryNum) {
+        int count = 0;
+        for (int i = 0; i < binaryNum.length(); i++) {
+            if (binaryNum.charAt(i) - '0' == 1) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution(78)); // 83
+    }
+}
