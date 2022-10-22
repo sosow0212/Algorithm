2901c73a7ee51eadaa88126306c77661a0c047d3
sosow0212 <sosow0212@naver.com>
2022-10-22 14:50:19 +0900

    .

---
+++ src/com/sosow0212/프로그래머스/직사각형별찍기.java
@@ -0,0 +1,18 @@
+package com.sosow0212.프로그래머스;
+
+import java.util.Scanner;
+
+class 직사각형별찍기 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        for(int i=0; i<b; i++) {
+            for(int j=0; j<a; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
