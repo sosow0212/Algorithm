8c6cef9fa05f7f22bd8ddc12d1cebfdb22fba1c3
sosow0212 <sosow0212@naver.com>
2022-11-03 14:41:09 +0900

    .

--- src/com/sosow0212/test/test.java
+++ src/com/sosow0212/test/test.java
@@ -1,18 +1,29 @@
 package com.sosow0212.test;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.Vector;
 
 public class test {
     public static void main(String[] args) {
-        String str = "제이엠";
-        String str2 = "제이슨";
-        List<Integer> answer = new ArrayList<>();
-        answer.add(1);
-        answer.add(5);
-        answer.add(3);
-        Collections.sort(answer, Comparator.reverseOrder());
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        List<Integer>[] graph = new ArrayList[n + 1];
 
-        System.out.println(str.substring(0,1));
+        for (int i = 0; i < n + 1; i++) { // i < m이 아니야 임마
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            graph[a].add(b);
+            graph[b].add(a);
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < graph[i].size(); j++) {
+                System.out.print(graph[i].get(j));
+            }
+            System.out.println();
+        }
 
     }
 }
---
+++ src/com/sosow0212/프로그래머스/나이출력.java
@@ -0,0 +1,11 @@
+package com.sosow0212.프로그래머스;
+
+class 나이출력 {
+    public int solution(int age) {
+        int answer = 0;
+
+        answer = 2023 - age;
+
+        return answer;
+    }
+}
