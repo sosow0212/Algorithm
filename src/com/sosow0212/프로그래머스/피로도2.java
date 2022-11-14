3fbecc2f608b239add470a76fa036fac7c3bebc5
sosow0212 <sosow0212@naver.com>
2022-11-14 10:47:16 +0900

    .

---
+++ src/com/sosow0212/프로그래머스/피로도2.java
@@ -0,0 +1,30 @@
+package com.sosow0212.프로그래머스;
+
+public class 피로도2 {
+    public static int answer;
+    public static boolean[] visited;
+
+    public static int solution(int k, int[][] dungeons) {
+        answer = 0;
+        visited = new boolean[dungeons.length];
+        backTracking(0, k, dungeons);
+        return answer;
+    }
+
+    public static void backTracking(int depth, int k, int[][] dungeons) {
+        for (int i = 0; i < dungeons.length; i++) {
+            if (k >= dungeons[i][0] && !visited[i]) {
+                visited[i] = true;
+                backTracking(depth + 1, k - dungeons[i][1], dungeons);
+                visited[i] = false;
+            }
+        }
+        answer = Math.max(answer, depth);
+    }
+
+    public static void main(String[] args) {
+        int k = 80;
+        int[][] dungeons = {{80, 20}, {50, 40}, {30, 10}};
+        System.out.println(solution(k, dungeons)); // 3
+    }
+}
