f8b7b9ccbe566c7ef47117795235b98627ae456d
sosow0212 <sosow0212@naver.com>
2022-12-21 12:55:20 +0900

    feat : 여행경로 (풀이 과정 순서 추가)

--- src/com/sosow0212/프로그래머스/여행경로.java
+++ src/com/sosow0212/프로그래머스/여행경로.java
@@ -10,19 +10,26 @@ public class 여행경로 {
     private static List<String> result = new ArrayList<>();
 
     public static String[] solution(String[][] tickets) {
+        // 1. 변수 세팅
         visited = new boolean[tickets.length];
+
+        // 2. 완전탐색 및 정렬
         dfs(0, "ICN", "ICN", tickets);
         Collections.sort(result);
+
+        // 3. 정답 출력
         String[] answer = result.get(0).split(" ");
         return answer;
     }
 
     public static void dfs(int idx, String start, String route, String[][] tickets) {
+        // 1. 탈출 조건 (비행기 티켓을 모두 사용한 경우)
         if (idx == tickets.length) {
             result.add(route);
             return;
         }
 
+        // 2. 백트래킹 알고리즘 사용
         for (int i = 0; i < tickets.length; i++) {
             if (tickets[i][0].equals(start) && !visited[i]) {
                 visited[i] = true;
