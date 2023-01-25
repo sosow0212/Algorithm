6cfb67f660e7357196a5063f8f13c9088e69cad1
sosow0212 <sosow0212@naver.com>
2023-01-25 17:04:48 +0900

    feat : q15686(dfs)

--- src/com/sosow0212/백준/q15686.java
+++ src/com/sosow0212/백준/q15686.java
@@ -1,26 +1,65 @@
 package com.sosow0212.백준;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class q15686 {
-
-    static int[][] graph;
-    static int n, m;
+    private static int n, m;
+    private static int[][] map;
+    private static List<int[]> house = new ArrayList<>();
+    private static List<int[]> chicken = new ArrayList<>();
+    private static List<int[]> choice = new ArrayList<>();
+    private static int result = Integer.MAX_VALUE;
+    private static boolean[] visited;
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
         n = sc.nextInt();
         m = sc.nextInt();
-
-        graph = new int[n][n];
+        map = new int[n][n];
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                graph[i][j] = sc.nextInt();
+                map[i][j] = sc.nextInt();
+                if (map[i][j] == 1) {
+                    house.add(new int[]{i, j});
+                }
+                if (map[i][j] == 2) {
+                    chicken.add(new int[]{i, j});
+                }
             }
         }
 
+        visited = new boolean[chicken.size()];
+        dfs(0, 0);
+        System.out.println(result);
+    }
+
+    private static void dfs(int depth, int start) {
+        if (depth == m) {
+            int sum = 0;
+            for (int[] h : house) {
+                int min = Integer.MAX_VALUE;
+                for (int[] c : choice) {
+                    int d = Math.abs(h[0] - c[0]) + Math.abs(h[1] - c[1]);
+                    min = Math.min(d, min);
+                }
+                sum += min;
+            }
+            result = Math.min(result, sum);
+            return;
+        }
+
+        for (int i = start; i < chicken.size(); i++) {
+            if (!visited[i]) {
+                visited[i] = true;
+                choice.add(chicken.get(i));
+                dfs(depth + 1, i + 1);
+                choice.remove(choice.size() - 1);
+                visited[i] = false;
+            }
+        }
 
     }
 }
