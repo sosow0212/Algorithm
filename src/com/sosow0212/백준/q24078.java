2cc39aa94d160056ff3ec0ea7326f822c94557ab
sosow0212 <sosow0212@naver.com>
2023-03-10 19:55:04 +0900

    feat: q24078

---
+++ src/com/sosow0212/백준/q24078.java
@@ -0,0 +1,12 @@
+package com.sosow0212.백준;
+
+import java.util.Scanner;
+
+public class q24078 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(n % 21);
+    }
+}
