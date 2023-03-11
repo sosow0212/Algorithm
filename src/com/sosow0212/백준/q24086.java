20f193d7c2dfe6943b2d789e588aef666cb0a6e5
sosow0212 <sosow0212@naver.com>
2023-03-11 19:43:25 +0900

    feat: q24086

---
+++ src/com/sosow0212/백준/q24086.java
@@ -0,0 +1,15 @@
+package com.sosow0212.백준;
+
+import java.util.Scanner;
+
+public class q24086 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println(b - a);
+    }
+}
