45cecca3d53e8d5faef59be0f4a3bf6a9f2801f8
sosow0212 <sosow0212@naver.com>
2023-03-13 14:39:14 +0900

    feat: q5524

---
+++ src/com/sosow0212/백준/q5524.java
@@ -0,0 +1,16 @@
+package com.sosow0212.백준;
+
+import java.util.Scanner;
+
+public class q5524 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            System.out.println(sc.next().toLowerCase());
+        }
+    }
+}
