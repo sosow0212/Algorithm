344017a93486d02b33d098b1a488b9f3f779fa58
sosow0212 <sosow0212@naver.com>
2023-03-21 20:48:11 +0900

    feat: q15664

--- src/com/sosow0212/백준/q15664.java
+++ src/com/sosow0212/백준/q15664.java
@@ -1,18 +1,17 @@
 package com.sosow0212.백준;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
 
 public class q15664 {
 
-    private static final Scanner sc = new Scanner(System.in);
     private static int n, m;
+    private static int[] givenArr;
+    private static int[] answerArr;
     private static boolean[] visited;
-    private static List<int[]> answerList = new ArrayList<>();
-    private static int[] givenValues;
-    private static int[] answerValues;
+    private static Set<String> answer = new HashSet<>();
 
     public static void main(String[] args) {
         initData();
@@ -20,44 +19,47 @@ public class q15664 {
     }
 
     private static void initData() {
+        Scanner sc = new Scanner(System.in);
+
         n = sc.nextInt();
         m = sc.nextInt();
 
-        givenValues = new int[n];
+        givenArr = new int[n];
+        answerArr = new int[m];
         visited = new boolean[n];
 
-        answerValues = new int[m];
-
         for (int i = 0; i < n; i++) {
-            givenValues[i] = sc.nextInt();
+            givenArr[i] = sc.nextInt();
         }
 
-        Arrays.sort(givenValues);
+        Arrays.sort(givenArr);
     }
 
     private static void getAnswer() {
-        dfs(0);
-
-        for (int[] arr : answerList) {
-            for (int i = 0; i < arr.length; i++) {
-                System.out.print(arr[i] + " ");
-            }
-            System.out.println();
-        }
+        permutation(0);
+        System.out.println(answer);
     }
 
-    private static void dfs(int index) {
+    private static void permutation(int index) {
         if (index == m) {
-            Arrays.sort(answerValues);
-            answerList.add(answerValues.clone());
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < answerArr.length - 1; i++) {
+                if(answerArr[i] <= answerArr[i+1]) {
+                    sb.append(String.valueOf(i) + " ");
+                }
+            }
+
+            if(sb.toString().trim().split(" ").length == m) {
+                answer.add(sb.toString());
+            }
             return;
         }
 
-        for (int i = 0; i < givenValues.length; i++) {
+        for (int i = 0; i < givenArr.length - 1; i++) {
             if (!visited[i]) {
+                answerArr[index] = givenArr[i];
                 visited[i] = true;
-                answerValues[index] = givenValues[i];
-                dfs(index + 1);
+                permutation(index + 1);
                 visited[i] = false;
             }
         }
