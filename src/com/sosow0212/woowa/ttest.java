8fe74dd9853e020cdbb77b602ad0791ac3ef56c4
sosow0212 <sosow0212@naver.com>
2023-05-12 22:59:27 +0900

    feat: 학습

--- src/com/sosow0212/woowa/ttest.java
+++ src/com/sosow0212/woowa/ttest.java
@@ -8,8 +8,8 @@ public class ttest {
                 List.of("jm@email.com", "제이엠"),
                 List.of("jason@email.com", "제이슨"),
                 List.of("woniee@email.com", "워니"),
-                List.of("mj@email.com", "엠제이"),
-                List.of("nowm@email.com", "이제엠")
+                List.of("mj@email.com", "엠제이")
+//                List.of("nowm@email.com", "이제엠")
         );
 
         List<String> result = List.of("jason@email.com", "jm@email.com", "mj@email.com");
