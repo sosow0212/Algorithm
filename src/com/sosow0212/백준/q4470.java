a7e8e1f4977063ccff2f76064ad3aa17562bda59
sosow0212 <sosow0212@naver.com>
2023-06-02 17:05:50 +0900

    feat: q4470

---
+++ src/com/sosow0212/백준/q4470.java
@@ -0,0 +1,20 @@
+package com.sosow0212.백준;
+
+import java.util.Scanner;
+
+public class q4470 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        sc.nextLine();
+
+        String[] arr = new String[N];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextLine();
+            System.out.println((i + 1) + ". " + arr[i]);
+        }
+        sc.close();
+    }
+
+}
