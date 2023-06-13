41f607920c0a27f692d821a9d15ed536164c649c
sosow0212 <sosow0212@naver.com>
2023-06-13 16:44:39 +0900

    feat: 백준 우선순위큐 시리즈

---
+++ src/com/sosow0212/백준/q1927.java
@@ -0,0 +1,32 @@
+package com.sosow0212.백준;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+
+public class q1927 {
+
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        for (int i = 0; i < n; i++) {
+            int num = Integer.parseInt(br.readLine());
+
+            if (num == 0) {
+                if (pq.isEmpty()) {
+                    System.out.println(0);
+                } else {
+                    System.out.println(pq.poll());
+                }
+
+                continue;
+            }
+
+            pq.add(num);
+        }
+    }
+}
