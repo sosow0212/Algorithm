34d4539ec788b08f0dc73ed08c054822cbf948eb
sosow0212 <sosow0212@naver.com>
2023-06-22 16:35:59 +0900

    feat: 숫자변환하기

---
+++ src/com/sosow0212/프로그래머스/숫자변환하기.java
@@ -0,0 +1,22 @@
+package com.sosow0212.프로그래머스;
+
+public class 숫자변환하기 {
+
+    public int solution(int x, int y, int n) {
+        int answer = 0;
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        숫자변환하기 sol = new 숫자변환하기();
+
+        // 2
+        System.out.println(sol.solution(10, 40, 5));
+
+        // 1
+//        System.out.println(sol.solution(10, 40, 30));
+
+        // -1
+//        System.out.println(sol.solution(2, 5, 4));
+    }
+}
