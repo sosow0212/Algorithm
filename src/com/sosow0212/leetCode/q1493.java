ac195116eb656b6717b6650ae08c939dcfe7eec0
sosow0212 <sosow0212@naver.com>
2023-07-05 10:04:54 +0900

    feat: leetCode Daily : 1493 (슬라이딩 윈도우)

---
+++ src/com/sosow0212/leetCode/q1493.java
@@ -0,0 +1,36 @@
+package com.sosow0212.leetCode;
+
+public class q1493 {
+
+    public int longestSubarray(int[] nums) {
+        int answer = 0;
+
+        int start = 0;
+        int zero = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 0) {
+                zero++;
+            }
+
+            while (zero > 1) {
+                if (nums[start] == 0) {
+                    zero -= 1;
+                }
+
+                start++;
+            }
+
+            answer = Math.max(answer, i - start);
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        q1493 sol = new q1493();
+        int[] nums = {1, 1, 0, 1};
+        // 3
+        System.out.println(sol.longestSubarray(nums));
+    }
+}
