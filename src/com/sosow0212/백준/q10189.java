d6166843943a70e7000c75aa2d891c203ee1266f
sosow0212 <sosow0212@naver.com>
2023-09-03 16:24:39 +0900

    feat: q10189

---
+++ src/com/sosow0212/백준/q10189.java
@@ -0,0 +1,10 @@
+package com.sosow0212.백준;
+
+public class q10189 {
+    public static void main(String[] args) {
+        System.out.println("#  # #### #### #  #\n" +
+                "#### #  # #  # # #\n" +
+                "#### #  # #  # # #\n" +
+                "#  # #### #### #  #");
+    }
+}
