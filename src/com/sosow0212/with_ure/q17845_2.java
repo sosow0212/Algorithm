4b921c1dd9982ed100358a0508fa83863ba039c0
sosow0212 <sosow0212@naver.com>
2023-10-28 20:27:18 +0900

    feat: 코테 준비 day15

---
+++ src/com/sosow0212/with_ure/q17845_2.java
@@ -0,0 +1,31 @@
+package com.sosow0212.with_ure;
+
+import java.util.Scanner;
+
+public class q17845_2 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        int[][] dp = new int[n + 1][m + 1];
+
+        for (int i = 1; i <= n; i++) {
+            int w = sc.nextInt();
+            int v = sc.nextInt();
+
+            for (int j = 1; j <= m; j++) {
+                if (w > j) {
+                    dp[i][j] = dp[i - 1][j];
+                    continue;
+                }
+
+                dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - w] + v);
+            }
+        }
+
+        System.out.println(dp[n][m]);
+    }
+}
