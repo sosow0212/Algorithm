bc090188ef4d5efac48c7b853d81948fa55600cb
sosow0212 <sosow0212@naver.com>
2023-11-02 17:40:24 +0900

    feat: 코테 준비 day20 (파일정리)

--- src/com/sosow0212/백준/q17388.py
+++
@@ -1,6 +0,0 @@
-S, K, H = map(int, input().split())
-d = {S: "Soongsil", K: "Korea", H: "Hanyang"}
-if sum(d) >= 100:
-    print("OK")
-else:
-    print(d[min(d)])
--- src/com/sosow0212/백준/q2729.py
+++
@@ -1,6 +0,0 @@
-n = int(input())
-for x in range(n):
-	a, b = input().split()
-	a, b = int(f"0b{a}", 2), int(f"0b{b}", 2)
-	result = bin(a + b)[2:]
-	print(result)
