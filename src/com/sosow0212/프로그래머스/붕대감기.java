8bce7a20106aaa8cd4cbf5afab07fa6e0f36a58c
sosow0212 <sosow0212@naver.com>
2023-11-22 17:31:54 +0900

    feat: 코테 준비 day29 - pccp

---
+++ src/com/sosow0212/프로그래머스/붕대감기.java
@@ -0,0 +1,62 @@
+package com.sosow0212.프로그래머스;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class 붕대감기 {
+
+    public static int solution(int[] bandage, int health, int[][] attacks) {
+        int answer = 0;
+
+        Queue<int[]> attackQueue = new LinkedList<>(Arrays.asList(attacks));
+        int needTime = bandage[0];
+        int healForTime = bandage[1];
+        int bonusHeal = bandage[2];
+
+        int nowHealth = health;
+        int successCount = 0;
+
+        int time = 0;
+
+        while (!attackQueue.isEmpty() && nowHealth >= 1) {
+            time++;
+
+            if (time == attackQueue.peek()[0]) {
+                int[] attack = attackQueue.poll();
+                nowHealth -= attack[1];
+                successCount = 0;
+                continue;
+            }
+
+            successCount++;
+            if (nowHealth != health) {
+                nowHealth += healForTime;
+            }
+
+            if (needTime == successCount) {
+                nowHealth += bonusHeal;
+                successCount = 0;
+            }
+
+            if (nowHealth >= health) {
+                nowHealth = health;
+            }
+        }
+
+        if (nowHealth <= 0) {
+            return -1;
+        }
+
+        return nowHealth;
+    }
+
+    public static void main(String[] args) {
+        int[] bandage = {1, 1, 1};
+        int health = 5;
+        int[][] attacks = {{1, 2}, {3, 2}};
+
+        // 3
+        System.out.println(solution(bandage, health, attacks));
+    }
+}
---
+++ src/com/sosow0212/프로그래머스/시추관.java
@@ -0,0 +1,72 @@
+package com.sosow0212.프로그래머스;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class 시추관 {
+
+    static int[][] pos = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+    static Map<Integer, Integer> oilOfCol;
+    static boolean[][] visited;
+    static int n;
+    static int m;
+    static int oil;
+    static Set<Integer> colSet;
+
+    public static int solution(int[][] land) {
+        int answer = 0;
+        n = land.length;
+        m = land[0].length;
+
+        oilOfCol = new HashMap<>();
+        visited = new boolean[n][m];
+        colSet = new LinkedHashSet<>();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (!visited[i][j] && land[i][j] == 1) {
+                    oil = 0;
+                    colSet = new HashSet<>();
+                    dfs(i, j, land);
+
+                    for (Integer col : colSet) {
+                        oilOfCol.put(col, oilOfCol.getOrDefault(col, 0) + oil);
+                    }
+                }
+            }
+        }
+
+        for (Integer value : oilOfCol.values()) {
+            answer = Math.max(value, answer);
+        }
+
+        return answer;
+    }
+
+    static void dfs(int row, int col, int[][] land) {
+        visited[row][col] = true;
+        oil++;
+        colSet.add(col);
+
+        for (int i = 0; i < pos.length; i++) {
+            int nr = row + pos[i][0];
+            int nc = col + pos[i][1];
+
+            if (nr >= 0 && nc >= 0 && nr < n && nc < m
+                    && !visited[nr][nc]
+                    && land[nr][nc] == 1) {
+                dfs(nr, nc, land);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[][] land = {{0, 0, 0, 1, 1, 1, 0, 0}, {0, 0, 0, 0, 1, 1, 0, 0}, {1, 1, 0, 0, 0, 1, 1, 0}, {1, 1, 1, 0, 0, 0, 0, 0}, {1, 1, 1, 0, 0, 0, 1, 1}};
+
+        // 9
+        System.out.println(solution(land));
+    }
+}
