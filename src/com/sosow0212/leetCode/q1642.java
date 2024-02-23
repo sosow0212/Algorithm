d7fef566b2e9542034ee6d3af196089026c0588d
sosow0212 <sosow0212@naver.com>
2024-02-23 11:47:30 +0900

    feat: 코테 준비 day104 - leetcode : 그리디(힙), 구현

--- src/com/sosow0212/leetCode/q1642.java
+++ src/com/sosow0212/leetCode/q1642.java
@@ -1,61 +1,46 @@
 package com.sosow0212.leetCode;
 
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.Comparator;
+import java.util.PriorityQueue;
 
 public class q1642 {
 
-    static int n;
-    static int m;
-    static int[][] dp;
-    static boolean[][] visited;
-    static int[][] pos = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+    public int furthestBuilding(int[] heights, int bricks, int ladders) {
+        PriorityQueue<Integer> diff = new PriorityQueue<>(Comparator.reverseOrder());
 
-    public static int solution(int[][] maps) {
         int answer = 0;
 
-        n = maps.length;
-        m = maps[0].length;
-        dp = new int[n][m];
-        visited = new boolean[n][m];
+        int i = 0;
+        for (; i < heights.length - 1; i++) {
+            int step = heights[i + 1] - heights[i];
 
-        for (int[] ints : dp) {
-            Arrays.fill(ints, Integer.MIN_VALUE);
-        }
+            if (step <= 0) {
+                continue;
+            }
 
-        dp[0][0] = 1;
-        bfs(maps);
+            diff.add(step);
+            bricks -= step;
 
-        return dp[n - 1][m - 1] == Integer.MIN_VALUE ? -1 : dp[n - 1][m - 1];
-    }
+            if (bricks < 0) {
+                bricks += diff.poll();
+                ladders--;
+            }
 
-    static void bfs(final int[][] maps) {
-        Queue<int[]> q = new LinkedList<>();
-        visited[0][0] = true;
-        q.add(new int[]{0, 0});
-
-        while (!q.isEmpty()) {
-            int[] now = q.poll();
-
-            for (int i = 0; i < pos.length; i++) {
-                int nr = now[0] + pos[i][0];
-                int nc = now[1] + pos[i][1];
-
-                if (!(nr >= 0 && nr < n && nc >= 0 && nc < m && !visited[nr][nc] && maps[nr][nc] == 1)) {
-                    continue;
-                }
-
-                visited[nr][nc] = true;
-                dp[nr][nc] = Math.max(dp[nr][nc], dp[now[0]][now[1]] + 1);
-                q.add(new int[]{nr, nc});
+            if (ladders < 0) {
+                break;
             }
         }
+
+        return i;
     }
 
     public static void main(String[] args) {
-        int[][] maps = {{1, 0, 1, 1, 1}, {1, 0, 1, 0, 1}, {1, 0, 1, 1, 1}, {1, 1, 1, 0, 1}, {0, 0, 0, 0, 1}};
+        q1642 sol = new q1642();
 
-        System.out.println(solution(maps));
+        // 4
+        System.out.println(sol.furthestBuilding(new int[]{4, 2, 7, 6, 9, 14, 12}, 5, 1));
+
+        // 7
+        System.out.println(sol.furthestBuilding(new int[]{4, 12, 2, 7, 3, 18, 20, 3, 19}, 10, 2));
     }
 }
---
+++ src/com/sosow0212/leetCode/q49.java
@@ -0,0 +1,103 @@
+package com.sosow0212.leetCode;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class q49 {
+
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+
+        for (String str : strs) {
+            char[] temp = str.toCharArray();
+            Arrays.sort(temp);
+            String sortedStr = String.valueOf(temp);
+
+            map.putIfAbsent(sortedStr, new ArrayList<String>());
+            map.get(sortedStr).add(str);
+        }
+
+        return new ArrayList<>(map.values());
+    }
+
+    /**
+     * // FAIL(MEMORY) BACKTRACKS
+     *
+     *     static List<List<String>> answer;
+     *     static int n;
+     *     static boolean[] visited;
+     *
+     *     public List<List<String>> groupAnagrams(String[] strs) {
+     *         answer = new ArrayList<>();
+     *         n = strs.length;
+     *         visited = new boolean[n];
+     *
+     *         for (int i = 0; i < strs.length; i++) {
+     *             if (visited[i]) {
+     *                 continue;
+     *             }
+     *
+     *             String str = strs[i];
+     *             List<String> list = new ArrayList<>();
+     *             Map<Character, Integer> map = new HashMap<>();
+     *             for (char c : str.toCharArray()) {
+     *                 map.put(c, map.getOrDefault(c, 0) + 1);
+     *             }
+     *
+     *             list.add(str);
+     *             visited[i] = true;
+     *             permutation(str, list, strs, map);
+     *             answer.add(list);
+     *         }
+     *
+     *         return answer;
+     *     }
+     *
+     *     static void permutation(final String str, final List<String> list, final String[] strs, final Map<Character, Integer> map) {
+     *         for (int i = 0; i < n; i++) {
+     *             if (visited[i]) {
+     *                 continue;
+     *             }
+     *
+     *             String node = strs[i];
+     *             boolean isContinue = false;
+     *
+     *             HashMap<Character, Integer> copyMap = new HashMap<>(map);
+     *
+     *             for (char c : node.toCharArray()) {
+     *                 if (!copyMap.containsKey(c)) {
+     *                     isContinue = true;
+     *                     break;
+     *                 }
+     *
+     *                 copyMap.put(c, copyMap.get(c) - 1);
+     *                 if (copyMap.get(c) == 0) {
+     *                     copyMap.remove(c);
+     *                 }
+     *             }
+     *
+     *             if (isContinue) {
+     *                 continue;
+     *             }
+     *
+     *             if (copyMap.keySet().isEmpty()) {
+     *                 visited[i] = true;
+     *                 list.add(strs[i]);
+     *             }
+     *         }
+     *     }
+     */
+
+    public static void main(String[] args) {
+        q49 sol = new q49();
+
+        // Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+        System.out.println(sol.groupAnagrams(new String[]{"eat", "tea", "tan", "ate", "nat", "bat"}));
+
+        // [["tittt","tttit","tttti"],["hhhhu","hhhuh","hhuhh"]]
+        System.out.println(sol.groupAnagrams(new String[]{"hhhhu", "tttti", "tttit", "hhhuh", "hhuhh", "tittt"}));
+    }
+}
