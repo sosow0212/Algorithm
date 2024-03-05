0170a0c66593de786f51d4ff561a49fc7b11401c
sosow0212 <sosow0212@naver.com>
2024-03-05 13:22:12 +0900

    feat: 코테 준비 day113 - 백트래킹

---
+++ src/com/sosow0212/woowa_study/set3/q28075_2.java
@@ -0,0 +1,53 @@
+package com.sosow0212.woowa_study.set3;
+
+import java.util.Scanner;
+
+public class q28075_2 {
+
+    static int n;
+    static int m;
+    static int[][] arr;
+    static int answer;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        n = sc.nextInt();
+        m = sc.nextInt();
+        arr = new int[2][3];
+        answer = 0;
+
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 3; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+
+        permutation(0, 0, -1);
+        System.out.println(answer);
+    }
+
+    static void permutation(int index, int sum, int beforeCol) {
+        if (index == n) {
+            if (sum >= m) {
+                answer++;
+            }
+
+            return;
+        }
+
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 3; j++) {
+                int score = 0;
+
+                if (beforeCol == j) {
+                    score += (arr[i][j] / 2);
+                } else {
+                    score = arr[i][j];
+                }
+
+                permutation(index + 1, sum + score, j);
+            }
+        }
+    }
+}
