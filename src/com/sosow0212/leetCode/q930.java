c08d32bc7124c66c28e9736b72c03ad0a470578d
sosow0212 <sosow0212@naver.com>
2024-03-14 17:38:30 +0900

    feat: 코테 준비 day122 - 구현

---
+++ src/com/sosow0212/leetCode/q930.java
@@ -0,0 +1,27 @@
+package com.sosow0212.leetCode;
+
+import java.util.HashMap;
+
+public class q930 {
+
+    public static int numSubarraysWithSum(int[] nums, int goal) {
+        int answer = 0;
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
+
+        int prefix = 0;
+        for (int num : nums) {
+            prefix += num;
+            answer += map.getOrDefault(prefix - goal, 0);
+            map.put(prefix, map.getOrDefault(prefix, 0) + 1);
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        // 3
+        System.out.println(numSubarraysWithSum(new int[]{1, 2, 2, 1, 2}, 5));
+    }
+}
