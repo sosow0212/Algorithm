d54974696266485aecf5cad9dadfb1f5c8178b93
sosow0212 <sosow0212@naver.com>
2024-03-26 17:05:28 +0900

    feat: 코테 준비 day132 - 자료구조

---
+++ src/com/sosow0212/백준/q11256.java
@@ -0,0 +1,46 @@
+package com.sosow0212.백준;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.Scanner;
+
+public class q11256 {
+
+    static final Scanner sc = new Scanner(System.in);
+    static final StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) {
+        int t = sc.nextInt();
+        for (int i = 0; i < t; i++) {
+            solve();
+        }
+
+        System.out.println(sb.toString());
+    }
+
+    static void solve() {
+        int candy = sc.nextInt();
+        int m = sc.nextInt();
+
+        int[] arr = new int[m];
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
+        for (int i = 0; i < m; i++) {
+            arr[i] = sc.nextInt() * sc.nextInt();
+            pq.add(arr[i]);
+        }
+
+        int answer = 0;
+        while (true) {
+            Integer poll = pq.poll();
+            candy -= poll;
+            answer++;
+
+            if (candy <= 0) {
+                break;
+            }
+        }
+
+        sb.append(answer)
+                .append("\n");
+    }
+}
