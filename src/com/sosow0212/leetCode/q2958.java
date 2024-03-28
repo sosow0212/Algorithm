d94697ba4654f87a0570c70ecfce86030bbbe0e1
sosow0212 <sosow0212@naver.com>
2024-03-28 17:16:41 +0900

    feat: 코테 준비 day134 - bfs, 시뮬레이션

---
+++ src/com/sosow0212/leetCode/q2958.java
@@ -0,0 +1,29 @@
+package com.sosow0212.leetCode;
+
+import java.util.HashMap;
+
+public class q2958 {
+
+    public static int maxSubarrayLength(int[] nums, int k) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int l = 0;
+        int answer = 0;
+
+        for (int r = 0; r < nums.length; r++) {
+            map.put(nums[r], map.getOrDefault(nums[r], 0) + 1);
+
+            while (map.get(nums[r]) > k) {
+                map.put(nums[l], map.get(nums[l]) - 1);
+                l++;
+            }
+
+            answer = Math.max(answer, r - l + 1);
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(maxSubarrayLength(new int[]{1, 2, 3, 1, 2, 3, 1, 2}, 2));
+    }
+}
