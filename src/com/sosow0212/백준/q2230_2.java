1c412e3275536d617992cb801423248702d87660
sosow0212 <sosow0212@naver.com>
2024-04-13 19:28:58 +0900

    feat: 코테 준비 day147 - 투포인터 모음

---
+++ src/com/sosow0212/백준/q2230_2.java
@@ -0,0 +1,38 @@
+package com.sosow0212.백준;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class q2230_2 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        Arrays.sort(arr);
+
+        int left = 0;
+        int right = 0;
+        int answer = Integer.MAX_VALUE;
+
+        while (left <= right && right < n) {
+            int sum = arr[right] - arr[left];
+
+            if (sum >= m) {
+                answer = Math.min(answer, sum);
+                left++;
+            } else {
+                right++;
+            }
+        }
+
+        System.out.println(answer);
+    }
+}
