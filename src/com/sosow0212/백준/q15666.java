3e88fd50f6d573ec6dadc5733fc876d10b796864
sosow0212 <sosow0212@naver.com>
2024-05-01 14:23:51 +0900

    feat: 코테 준비 day163 - 백트래킹, 그리디

---
+++ src/com/sosow0212/백준/q15666.java
@@ -0,0 +1,49 @@
+package com.sosow0212.백준;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class q15666 {
+
+    static int[] arr;
+    static int n;
+    static int m;
+    static List<String> list;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        m = sc.nextInt();
+        list = new ArrayList<>();
+
+        arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        Arrays.sort(arr);
+        permutation(0, 0, "");
+        StringBuilder sb = new StringBuilder();
+
+        for (String s : list) {
+            sb.append(s).append("\n");
+        }
+
+        System.out.println(sb.toString());
+    }
+
+    static void permutation(int index, int start, String sum) {
+        if (index == m) {
+            if (!list.contains(sum)) {
+                list.add(sum);
+            }
+            return;
+        }
+
+        for (int i = start; i < n; i++) {
+            permutation(index + 1, i, sum + arr[i] + " ");
+        }
+    }
+}
