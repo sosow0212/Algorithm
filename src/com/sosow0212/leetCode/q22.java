57fba9606278452f0271a800c2830d714e949c5d
sosow0212 <sosow0212@naver.com>
2024-05-03 13:54:36 +0900

    feat: 코테 준비 day165 - 문자열, 백트래킹

---
+++ src/com/sosow0212/leetCode/q22.java
@@ -0,0 +1,57 @@
+package com.sosow0212.leetCode;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class q22 {
+
+    static int m;
+    static List<String> result;
+
+    public static List<String> generateParenthesis(int n) {
+        result = new ArrayList<>();
+        m = n * 2;
+        permutation(1, "(");
+        return result;
+    }
+
+    static void permutation(int index, String sum) {
+        if (index == m) {
+            if (isPair(sum)) {
+                result.add(sum);
+            }
+            return;
+        }
+
+        permutation(index + 1, sum + "(");
+        permutation(index + 1, sum + ")");
+    }
+
+    private static boolean isPair(final String sum) {
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < sum.length(); i++) {
+            char c = sum.charAt(i);
+
+            if (stack.isEmpty()) {
+                stack.push(c);
+                continue;
+            }
+
+            if (stack.peek() == '(' && c == ')') {
+                stack.pop();
+            } else {
+                stack.push(c);
+            }
+        }
+
+        return stack.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        //          [((())), (()()), (())(), ()(()), ()()()]
+        // Output: ["((()))","(()())","(())()","()(())","()()()"]
+        System.out.println(generateParenthesis(3));
+    }
+}
