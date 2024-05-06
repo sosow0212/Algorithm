58affb13699a8e348a8563912c22d10453426a01
sosow0212 <sosow0212@naver.com>
2024-05-06 18:53:43 +0900

    feat: 코테 준비 day168 - dp, 그리디

---
+++ src/com/sosow0212/leetCode/q347.java
@@ -0,0 +1,30 @@
+package com.sosow0212.leetCode;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+
+public class q347 {
+
+    public int[] topKFrequent(int[] nums, int k) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i : nums) {
+            map.put(i, map.getOrDefault(i, 0) + 1);
+        }
+
+        List<Integer> keys = new ArrayList<>(map.keySet());
+        Collections.sort(keys, (o1, o2) -> {
+            return map.get(o2) - map.get(o1);
+        });
+
+        // 1 2 3
+        int[] answer = new int[k];
+        for (int i = 0; i < k; i++) {
+            answer[i] = keys.get(i);
+        }
+
+        return answer;
+    }
+}
