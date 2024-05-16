e6d7d60518ddcec079855dbc596e0cefd127423d
sosow0212 <sosow0212@naver.com>
2024-05-16 10:28:06 +0900

    feat: 코테 준비 day177 - bin-tree

---
+++ src/com/sosow0212/leetCode/q2331.java
@@ -0,0 +1,19 @@
+package com.sosow0212.leetCode;
+
+public class q2331 {
+//
+//    class Solution {
+//        public boolean evaluateTree(TreeNode root) {
+//            if (func(root)==1) return true;
+//            return false;
+//        }
+//        int func (TreeNode root) {
+//            if (root.left==null && root.right==null) return root.val;
+//            int left = func(root.left);
+//            int right = func(root.right);
+//            if (root.val==2) return left | right;
+//            if (root.val==3) return left & right;
+//            return root.val;
+//        }
+//    }
+}
