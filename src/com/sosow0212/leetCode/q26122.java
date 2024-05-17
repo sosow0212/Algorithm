5bd1cfc458c0dc038eda719b3d4c09baaa88ad69
sosow0212 <sosow0212@naver.com>
2024-05-17 17:02:00 +0900

    feat: day178 - 완탐 시간초과 -> 스택 O(N) / 코드 가독성 개선 필요할듯

---
+++ src/com/sosow0212/leetCode/q26122.java
@@ -0,0 +1,57 @@
+package com.sosow0212.leetCode;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.Stack;
+
+public class q26122 {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int k = sc.nextInt();
+        String s = sc.next();
+
+        Stack<Integer> stackN = new Stack<>();
+        Stack<Integer> stackS = new Stack<>();
+        ArrayList<Integer> temp = new ArrayList<>();
+
+        boolean flag = false;
+        for (int i = 0; i < k; i++) {
+            if (s.charAt(i) == 'N') {
+                stackN.push(1);
+                if (!flag) {
+                    temp.add(stackS.size());
+                    while (!stackS.isEmpty()) {
+                        stackS.pop();
+                    }
+                }
+                flag = true;
+            } else if (s.charAt(i) == 'S') {
+                stackS.push(1);
+                if (flag) {
+                    temp.add(stackN.size());
+                    while (!stackN.isEmpty()) {
+                        stackN.pop();
+                    }
+                }
+                flag = false;
+            }
+        }
+
+        if (flag) {
+            temp.add(stackN.size());
+        } else {
+            temp.add(stackS.size());
+        }
+
+        temp.add(0);
+
+        int res = 0;
+
+        for (int i = 1; i < temp.size(); i++) {
+            res = Math.max(res, 2 * Math.min(temp.get(i - 1), temp.get(i)));
+        }
+
+        System.out.println(res);
+    }
+}
