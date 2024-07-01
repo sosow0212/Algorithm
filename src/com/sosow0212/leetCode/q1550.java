28045c48c9d379327a0ebb37991a43cc255f2e69
sosow0212 <sosow0212@naver.com>
2024-07-01 09:36:03 +0900

    feat: day214 - 연속배열 조건찾기

---
+++ src/com/sosow0212/leetCode/q1550.java
@@ -0,0 +1,22 @@
+package com.sosow0212.leetCode;
+
+public class q1550 {
+
+    public boolean threeConsecutiveOdds(int[] arr) {
+        int count = 0;
+
+        for (int i : arr) {
+            if (i % 2 == 1) {
+                count++;
+            } else {
+                count = 0;
+            }
+
+            if (count == 3) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
