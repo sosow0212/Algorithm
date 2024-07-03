3cbb9f95d9c44099af3191bfac0e5cd462f7a5ee
sosow0212 <sosow0212@naver.com>
2024-07-03 12:06:51 +0900

    feat: day216 - 두 배열 value 갭 차이 최소화 문제 (그리디, 조금 어려웠음)

---
+++ src/com/sosow0212/leetCode/q1509.java
@@ -0,0 +1,52 @@
+package com.sosow0212.leetCode;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.PriorityQueue;
+
+public class q1509 {
+
+    public static int minDifference(int[] nums) {
+        if (nums.length <= 4) {
+            return 0;
+        }
+
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>(Comparator.reverseOrder());
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>();
+
+        for (int i : nums) {
+            maxHeap.add(i);
+            if (maxHeap.size() > 4) {
+                maxHeap.poll();
+            }
+
+            minHeap.add(i);
+            if (minHeap.size() > 4) {
+                minHeap.poll();
+            }
+        }
+
+        List<Integer> minList = new ArrayList<>(maxHeap);
+        Collections.sort(minList);
+
+        List<Integer> maxList = new ArrayList<>(minHeap);
+        Collections.sort(maxList);
+
+        int diff = Integer.MAX_VALUE;
+        for (int i = 0; i < 4; i++) {
+            diff = Math.min(diff, minList.get(i) - maxList.get(i));
+        }
+
+        return diff;
+    }
+
+    public static void main(String[] args) {
+        // 0
+//        System.out.println(minDifference(new int[]{5, 3, 2, 4}));
+
+        // 2
+        System.out.println(minDifference(new int[]{6, 6, 0, 1, 1, 4, 6}));
+    }
+}
