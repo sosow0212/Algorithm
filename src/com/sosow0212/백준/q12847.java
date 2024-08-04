e9b0bee2a0fa31c5438156ec9b46b54fa6251b80
sosow0212 <sosow0212@naver.com>
2024-08-04 16:59:11 +0900

    feat: day246 - 브루트포스

---
+++ src/com/sosow0212/백준/q12847.java
@@ -0,0 +1,35 @@
+package com.sosow0212.백준;
+
+import java.util.Scanner;
+
+public class q12847 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        long[] arr = new long[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        long sum = 0;
+        for (int i = 0; i < m; i++) {
+            sum += arr[i];
+        }
+
+        long answer = sum;
+        for (int i = 1; i < arr.length - m; i++) {
+            sum = 0;
+
+            for (int j = i; j < i + m; j++) {
+                sum += arr[j];
+            }
+
+            answer = Math.max(answer, sum);
+        }
+
+        System.out.println(answer);
+    }
+}
---
+++ src/com/sosow0212/백준/q1508.java
@@ -0,0 +1,43 @@
+package com.sosow0212.백준;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class q1508 {
+
+    public int rangeSum(int[] nums, int n, int left, int right) {
+        List<Integer> list = new ArrayList<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int sum = 0;
+
+            for (int j = i; j < nums.length; j++) {
+                sum += nums[j];
+                list.add(sum);
+            }
+        }
+
+        int mod = (int) 1e9 + 7;
+        Collections.sort(list);
+
+        int answer = 0;
+        for (int i = left - 1; i <= right - 1; i++) {
+            answer = (answer + list.get(i)) % mod;
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        q1508 sol = new q1508();
+
+        // 50
+        System.out.println(sol.rangeSum(
+                new int[]{1, 2, 3, 4},
+                4,
+                1,
+                10
+        ));
+    }
+}
