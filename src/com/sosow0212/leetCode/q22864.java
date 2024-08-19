3fdc4002df3958ef249a8f2ae05b98238d5fd55e
sosow0212 <sosow0212@naver.com>
2024-08-19 11:54:20 +0900

    feat: day260 - 그리디 모음

---
+++ src/com/sosow0212/leetCode/q22864.java
@@ -0,0 +1,36 @@
+package com.sosow0212.leetCode;
+
+import java.util.Scanner;
+
+public class q22864 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int consumeEnergy = sc.nextInt(); // 11
+        int workEfficient = sc.nextInt(); // 5
+        int chargeEnergy = sc.nextInt(); // 1
+        int m = sc.nextInt(); // 10
+
+        int time = 24;
+        int answer = 0;
+        int pirodo = 0;
+
+        while (time-- > 0) {
+            if (pirodo + consumeEnergy > m) {
+                pirodo -= chargeEnergy;
+
+                if (pirodo < 0) {
+                    pirodo = 0;
+                }
+
+                continue;
+            }
+
+            answer += workEfficient;
+            pirodo += consumeEnergy;
+        }
+
+        System.out.println(answer);
+    }
+}
