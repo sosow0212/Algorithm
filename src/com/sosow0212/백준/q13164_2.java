b69efa72ca603ddb5458b0d550cb34e23e6eb4fa
sosow0212 <sosow0212@naver.com>
2024-09-14 08:33:31 +0900

    feat: day285 - 그리디 시리즈 (구간 나누기, 가중 중앙값 찾기)

---
+++ src/com/sosow0212/백준/q13164_2.java
@@ -0,0 +1,44 @@
+package com.sosow0212.백준;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.Scanner;
+
+public class q13164_2 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        Arrays.sort(arr);
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
+
+        for (int i = 1; i < arr.length; i++) {
+            pq.add(arr[i] - arr[i - 1]);
+        }
+
+        // 1 3 5 6 10
+        //   2 2 1 4
+        //   1 2 2 4
+
+        int answer = 0;
+        while (k - 1 > 0) {
+            k--;
+            pq.poll();
+        }
+
+        while(!pq.isEmpty()) {
+            answer += pq.poll();
+        }
+
+        System.out.println(answer);
+    }
+}
