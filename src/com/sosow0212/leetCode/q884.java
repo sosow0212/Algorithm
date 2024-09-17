d65b7c0d7a9b927f783263d55857509f58b62a51
sosow0212 <sosow0212@naver.com>
2024-09-17 14:12:27 +0900

    feat: day287 - 문자열 리트코드

---
+++ src/com/sosow0212/leetCode/q884.java
@@ -0,0 +1,35 @@
+package com.sosow0212.leetCode;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class q884 {
+
+    public String[] uncommonFromSentences(String s1, String s2) {
+        HashMap<String, Integer> map = new HashMap<>();
+
+        for (String s : s1.split(" ")) {
+            map.put(s, map.getOrDefault(s, 0) + 1);
+        }
+
+        for (String s : s2.split(" ")) {
+            map.put(s, map.getOrDefault(s, 0) + 1);
+        }
+
+        List<String> list = new ArrayList<>();
+
+        for (String key : map.keySet()) {
+            if (map.get(key) == 1) {
+                list.add(key);
+            }
+        }
+
+        String[] answer = new String[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            answer[i] = list.get(i);
+        }
+
+        return answer;
+    }
+}
