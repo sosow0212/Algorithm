ee86528652042c33ade5b8074c59d619ee857833
sosow0212 <sosow0212@naver.com>
2024-10-06 22:02:02 +0900

    feat: day301 - 문자열 문제 (슬윈이 더 빠를듯)

---
+++ src/com/sosow0212/leetCode/q567.java
@@ -0,0 +1,24 @@
+package com.sosow0212.leetCode;
+
+import java.util.Arrays;
+
+public class q567 {
+
+    public boolean checkInclusion(String s1, String s2) {
+        s1 = sort(s1);
+
+        for (int i = 0; i <= s2.length() - s1.length(); i++) {
+            if (s1.equals(sort(s2.substring(i, i + s1.length())))) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private String sort(String s) {
+        char[] result = s.toCharArray();
+        Arrays.sort(result);
+        return new String(result);
+    }
+}
