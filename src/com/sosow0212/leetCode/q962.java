8cb206ee1e03a3e72a8848104a85a255129b9e72
sosow0212 <sosow0212@naver.com>
2024-10-10 12:02:12 +0900

    feat: day304 - 구현 (정렬) -> 뒷 수 찾기 / 다시 풀기

---
+++ src/com/sosow0212/leetCode/q962.java
@@ -0,0 +1,59 @@
+package com.sosow0212.leetCode;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class q962 {
+
+    class Node implements Comparable<Node> {
+        int index;
+        int value;
+
+        public Node(final int index, final int value) {
+            this.index = index;
+            this.value = value;
+        }
+
+        @Override
+        public int compareTo(final Node o) {
+            return this.value - o.value;
+        }
+
+        @Override
+        public String toString() {
+            return "Node{" +
+                    "index=" + index +
+                    ", value=" + value +
+                    '}';
+        }
+    }
+
+    public int maxWidthRamp(int[] nums) {
+        List<Node> list = new ArrayList<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            list.add(new Node(i, nums[i]));
+        }
+
+        Collections.sort(list);
+
+        int answer = 0;
+        int minIndex = Integer.MAX_VALUE;
+
+        for (Node node : list) {
+            int index = node.index;
+            minIndex = Math.min(minIndex, index);
+            answer = Math.max(answer, index - minIndex);
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        q962 sol = new q962();
+
+        // 4
+        System.out.println(sol.maxWidthRamp(new int[]{6, 0, 8, 2, 1, 5}));
+    }
+}
