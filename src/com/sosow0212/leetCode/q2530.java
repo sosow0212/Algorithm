6e029917ecc1c35b60fd5c172b4d8ccdbb5705f7
sosow0212 <sosow0212@naver.com>
2024-10-14 13:32:31 +0900

    feat: day307 - dfs / Heap

---
+++ src/com/sosow0212/leetCode/q2530.java
@@ -0,0 +1,34 @@
+package com.sosow0212.leetCode;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+public class q2530 {
+
+    public static long maxKelements(int[] nums, int k) {
+        long answer = 0;
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
+
+        for (int num : nums) {
+            pq.add(num);
+        }
+
+        while (k-- > 0 && !pq.isEmpty()) {
+            Integer now = pq.poll();
+
+            answer += now;
+            now = (int) Math.ceil((double) now / 3);
+            pq.add(now);
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        // 17
+//        System.out.println(maxKelements(new int[]{1, 10, 3, 3, 3}, 3));
+
+        // 2476180565
+        System.out.println(maxKelements(new int[]{672579538, 806947365, 854095676, 815137524}, 3));
+    }
+}
