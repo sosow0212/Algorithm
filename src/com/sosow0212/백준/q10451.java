1f2d1dd2508a09fa5802a3444fc144e84aa4d694
sosow0212 <sosow0212@naver.com>
2024-10-18 19:11:05 +0900

    feat: day311 - dfs / 백트래킹

---
+++ src/com/sosow0212/백준/q10451.java
@@ -0,0 +1,58 @@
+package com.sosow0212.백준;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class q10451 {
+
+    static final StringBuilder sb = new StringBuilder();
+    static final Scanner sc = new Scanner(System.in);
+    static ArrayList<Integer>[] list;
+    static boolean[] visited;
+
+    public static void main(String[] args) {
+        int t = sc.nextInt();
+
+        for (int i = 0; i < t; i++) {
+            solve();
+        }
+
+        System.out.println(sb.toString());
+    }
+
+    static void solve() {
+        int n = sc.nextInt();
+
+        list = new ArrayList[n + 1];
+        visited = new boolean[n + 1];
+
+        for (int i = 0; i <= n; i++) {
+            list[i] = new ArrayList<>();
+        }
+
+        for (int i = 1; i <= n; i++) {
+            int num = sc.nextInt();
+            list[i].add(num);
+        }
+
+        int count = 0;
+        for (int i = 1; i <= n; i++) {
+            if (!visited[i]) {
+                count++;
+                dfs(i);
+            }
+        }
+
+        sb.append(count).append("\n");
+    }
+
+    static void dfs(int node) {
+        visited[node] = true;
+
+        for (Integer i : list[node]) {
+            if (!visited[i]) {
+                dfs(i);
+            }
+        }
+    }
+}
