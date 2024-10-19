9cc4f36e291a601339c323ca68ab54f711a93fbb
sosow0212 <sosow0212@naver.com>
2024-10-19 09:11:31 +0900

    feat: day312 - 소프티어 (누적합, dp)

---
+++ src/com/sosow0212/softeer/성적평균.java
@@ -0,0 +1,32 @@
+package com.sosow0212.softeer;
+
+import java.util.Scanner;
+
+public class 성적평균 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
+
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int[] prefix = new int[n + 1];
+
+        for (int i = 1; i <= n; i++) {
+            prefix[i] = prefix[i - 1] + sc.nextInt();
+        }
+
+        for (int i = 0; i < k; i++) {
+            int start = sc.nextInt();
+            int end = sc.nextInt();
+
+            int sum = prefix[end] - prefix[start - 1];
+            double average = (double) sum / (end - start + 1);
+
+            sb.append(String.format("%.2f",(average)))
+                    .append("\n");
+        }
+
+        System.out.println(sb.toString());
+    }
+}
