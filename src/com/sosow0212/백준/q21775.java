37122467273f2170e2b7694168879b78c537e290
sosow0212 <sosow0212@naver.com>
2024-10-22 13:47:16 +0900

    feat: day315 - 완탐, 누적합

---
+++ src/com/sosow0212/백준/q21775.java
@@ -0,0 +1,63 @@
+package com.sosow0212.백준;
+
+import java.util.Scanner;
+
+public class q21775 {
+
+    static int n;
+    static int m;
+    static int t;
+    static char[][] map;
+    static int[] startPos;
+    static int answer;
+    static int[][] pos = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        m = sc.nextInt();
+        t = sc.nextInt();
+        map = new char[n][m];
+        startPos = new int[2];
+        answer = 0;
+
+        for (int i = 0; i < n; i++) {
+            String s = sc.next();
+
+            for (int j = 0; j < s.length(); j++) {
+                char c = s.charAt(j);
+
+                if (c == 'G') {
+                    startPos = new int[]{i, j};
+                }
+
+                map[i][j] = c;
+            }
+        }
+
+        permutation(0, startPos[0], startPos[1], 0);
+        System.out.println(answer);
+    }
+
+    static void permutation(int index, int row, int col, int sum) {
+        if (index == t) {
+            answer = Math.max(answer, sum);
+            return;
+        }
+
+        for (int i = 0; i < pos.length; i++) {
+            int nr = row + pos[i][0];
+            int nc = col + pos[i][1];
+
+            if (nr >= 0 && nr < n && nc >= 0 && nc < m && map[nr][nc] != '#') {
+                if (map[nr][nc] == 'S') {
+                    map[nr][nc] = '.';
+                    permutation(index + 1, nr, nc, sum + 1);
+                    map[nr][nc] = 'S';
+                } else {
+                    permutation(index + 1, nr, nc, sum);
+                }
+            }
+        }
+    }
+}
--- src/com/sosow0212/백준/q3020.java
+++ src/com/sosow0212/백준/q3020.java
@@ -11,30 +11,36 @@ public class q3020 {
         int n = sc.nextInt();
         int h = sc.nextInt();
 
-        int[] up = new int[h + 1];
-        int[] down = new int[h + 1];
+        int[] toUp = new int[h + 1];
+        int[] toDown = new int[h + 1];
 
-        int[] prefixUp = new int[h + 1];
-        int[] prefixDown = new int[h + 1];
-        int[] prefix = new int[h + 1];
-
-        // 돌의 길이만큼 카운터 저장
         for (int i = 0; i < n / 2; i++) {
-            down[sc.nextInt()]++;
-            up[sc.nextInt()]++;
+            int toUpNum = sc.nextInt();
+            int toDownNum = sc.nextInt();
+
+            toUp[toUpNum]++;
+            toDown[toDownNum]++;
         }
 
-        for (int i = 1; i < h + 1; i++) {
-            prefix[i] = up[i] + down[h + 1 - i];
+        for (int i = h - 1; i >= 1; i--) {
+            toUp[i] += toUp[i + 1];
+            toDown[i] += toDown[i + 1];
         }
 
+        int minObstacles = Integer.MAX_VALUE;
+        int count = 0;
 
-        System.out.println(Arrays.toString(up));
-        System.out.println(Arrays.toString(down));
-        System.out.println(Arrays.toString(prefix));
-        /**
-         * 14 5
-         * 1 3 4 2 2 4 3 4 3 3 3 2 3 4
-         */
+        for (int i = 1; i <= h; i++) {
+            int obstacles = toUp[i] + toDown[h - i + 1];
+
+            if (obstacles < minObstacles) {
+                minObstacles = obstacles;
+                count = 1;
+            } else if (obstacles == minObstacles) {
+                count++;
+            }
+        }
+
+        System.out.println(minObstacles + " " + count);
     }
 }
